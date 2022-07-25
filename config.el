5002394d9e2c81742b4a3aebc4d664273301274a
hvsouza <henriquevieira.souza@gmail.com>
2022-07-25 13:16:51 +0200

    My doom emacs configuration

---
+++ config.el
@@ -0,0 +1,159 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+(add-to-list 'bdf-directory-list "/usr/share/emacs/fonts/bdf")
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; best thing in emacs..
+(use-package! nyan-mode
+  :ensure t
+  :init
+  (setq nyan-animate-nyancat t)
+  :config
+  (nyan-mode)
+  (nyan-toggle-wavy-trail)
+  )
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 17)
+     doom-variable-pitch-font (font-spec :family "Ubuntu" :size 17))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+(setq evil-normal-state-cursor '(bar "yellow"))
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+(load "server")
+(unless (server-running-p) (server-start))
+
+;; to remove tabs and replace by space
+(setq-default indent-tabs-mode nil)
+
+;; ;; use-package which-key
+;;   :ensure t
+;;   :config (which-key-mode))
+
+;; To fix undo tree from showing on the left..
+(defadvice! +popup--use-popup-window-for-undo-tree-visualizer-a (fn &rest args)
+  :around #'undo-tree-visualize
+  (if undo-tree-visualizer-diff
+      (apply fn args)
+    (letf! ((#'switch-to-buffer-other-window #'pop-to-buffer))
+      (apply fn args))))
+
+; show parentheses stuff
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(setq blink-matching-paren 'show)
+;;(setq blink-matching-paren-distance nil)
+
+(use-package! smartparens
+:ensure t
+  :hook (prog-mode . smartparens-mode)
+  :custom
+  (sp-escape-quotes-after-insert nil)
+  :config
+  (require 'smartparens-config)
+  ;; :bind
+  ;; ;; set as C-M-f or b
+  ;; ("C-M-b" . sp-backward-sexp)
+  ;; ("C-M-f" . sp-forward-sexp)
+  ;; ("C-M-(" . sp-rewrap-sexp)
+  ;; ("C-M-)" . sp-splice-sexp)
+  )
+(show-paren-mode t)
+
+
+(use-package! beacon)
+(beacon-mode 1)
+
+;; Org mode table of content
+(use-package! org-make-toc
+  :ensure t)
+
+;; Org-mode stuff
+(use-package! org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; Org-mode export issue
+(setq org-export-with-broken-links 'mark)
+
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+;; setq highlight-indentation-overlay-string "|")
+;; (highlight-indentation-mode t)
+;; (add-hook 'org-mode-hook 'highlight-indentation-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'highlight-indentation-mode)
+;; (add-hook 'c++-mode-hook 'highlight-indentation-mode)
+;; (add-hook 'sh-mode-hook 'highlight-indentation-mode)
---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(undo-tree-visualizer-diff t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
